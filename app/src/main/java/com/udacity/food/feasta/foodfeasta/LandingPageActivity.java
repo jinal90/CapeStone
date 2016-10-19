ea3ba37ceaad954cfccefc5389da97785d9d8818
jinal90 <jinal010990@gmail.com>
2016-10-19 11:57:31 +0530

    draft menu fragment UI

--- app/src/main/java/com/udacity/food/feasta/foodfeasta/LandingPageActivity.java
+++ app/src/main/java/com/udacity/food/feasta/foodfeasta/LandingPageActivity.java
@@ -2,9 +2,9 @@ package com.udacity.food.feasta.foodfeasta;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,9 +12,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.udacity.food.feasta.foodfeasta.ui.MenuFragment;
+import com.udacity.food.feasta.foodfeasta.ui.dummy.DummyContent;
 
 public class LandingPageActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        MenuFragment.OnListFragmentInteractionListener {
+
+    private MenuFragment mMenuFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +39,21 @@ public class LandingPageActivity extends AppCompatActivity
             }
         });
 
+        loadFrament();
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    public void loadFrament() {
+        mMenuFragment = MenuFragment.newInstance(1);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.rlFragmentContainer, mMenuFragment).commit();
     }
 
     @Override
@@ -97,5 +111,10 @@ public class LandingPageActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
--- app/src/main/java/com/udacity/food/feasta/foodfeasta/ui/LoginScreenActivity.java
+++ app/src/main/java/com/udacity/food/feasta/foodfeasta/ui/LoginScreenActivity.java
@@ -1,18 +1,22 @@
 package com.udacity.food.feasta.foodfeasta.ui;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Interpolator;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import com.udacity.food.feasta.foodfeasta.LandingPageActivity;
 import com.udacity.food.feasta.foodfeasta.R;
-
-import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
 
 public class LoginScreenActivity extends BaseActivity {
@@ -20,6 +24,16 @@ public class LoginScreenActivity extends BaseActivity {
 
     @BindView(R.id.btnSubmit)
     Button btnSubmit;
+    @BindView(R.id.radioGroupUserType)
+    RadioGroup radioGroupUserType;
+    @BindView(R.id.radioCustomer)
+    RadioButton radioCustomer;
+    @BindView(R.id.radioManager)
+    RadioButton radioManager;
+    @BindView(R.id.edtName)
+    EditText edtInputName;
+    @BindView(R.id.edtTableNo)
+    EditText edtInputTableNo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +49,8 @@ public class LoginScreenActivity extends BaseActivity {
     @Override
     public void setupUI() {
 
+        edtInputName.setVisibility(View.GONE);
+        edtInputTableNo.setVisibility(View.GONE);
     }
 
     @Override
@@ -43,9 +59,40 @@ public class LoginScreenActivity extends BaseActivity {
     }
 
     @OnClick(R.id.btnSubmit)
-    public void onSubmitClick() {
+    public void onClick(View view) {
 
+        if (radioCustomer.isChecked() || radioManager.isChecked()) {
+            Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
 
+            Intent intent = new Intent(this, LandingPageActivity.class);
+            startActivity(intent);
+        } else {
+
+            Toast.makeText(this, "Fail", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @OnCheckedChanged({R.id.radioCustomer, R.id.radioManager})
+    public void onRadioCheckedChanged(CompoundButton view, boolean isChecked) {
+        switch (view.getId()) {
+            case R.id.radioCustomer:
+                if (isChecked) {
+                    edtInputName.setVisibility(View.GONE);
+                    edtInputTableNo.setVisibility(View.VISIBLE);
+                }
+                break;
+            case R.id.radioManager:
+                if (isChecked) {
+                    edtInputName.setVisibility(View.VISIBLE);
+                    edtInputTableNo.setVisibility(View.GONE);
+                }
+                break;
+
+            default:
+
+                break;
+        }
     }
 
     @Override
