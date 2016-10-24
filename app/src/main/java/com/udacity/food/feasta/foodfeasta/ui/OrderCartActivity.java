2ae973d2b05567d73fae208022b7732e92e1b1be
jinal90 <jinal010990@gmail.com>
2016-10-24 23:52:38 +0530

    created basic order cart activity

---
+++ app/src/main/java/com/udacity/food/feasta/foodfeasta/ui/OrderCartActivity.java
@@ -0,0 +1,15 @@
+package com.udacity.food.feasta.foodfeasta.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.udacity.food.feasta.foodfeasta.R;
+
+public class OrderCartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_cart);
+    }
+}
